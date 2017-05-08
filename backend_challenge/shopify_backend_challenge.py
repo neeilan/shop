33000f001aba696a60d77f4b04e051b362f12872
neeilan <neeilans@live.com>
2017-05-07 23:04:04 -0400

    refactored http request details into separate class

--- backend_challenge/shopify_backend_challenge.py
+++ backend_challenge/shopify_backend_challenge.py
@@ -1,48 +1,57 @@
 import urllib.request, json 
-
-
-    
-
-
-  
-    
-
-        
-api_url = 'https://backend-challenge-fall-2017.herokuapp.com/orders.json'
-
-print ( get_orders_on_page(api_url, 3) )
-
+import math
 
 class JSONFetcher():
-    def __init__():
-        pass
+    def __init__(self, base_url):
+        self.base_url = base_url
     
-    def fetch(query_url):
+    def fetch(self, query = {}):
+        query_url = self._build_query_url(query)
         with urllib.request.urlopen(query_url) as url:
             data = json.loads(url.read().decode())
             return data
+
+    def _build_query_url(self, query):
+        if query == {}:
+            query_url = self.base_url
+        else:
+            query_str = '/?'
+            for param in query:
+                query_str += '{0}={1}'.format(param, query[param])
+                query_url = self.base_url + query_str
+        return query_url
+        
         
 class ShopifyAPI():
     
     def __init__(self, api_url):
         self.api_url = api_url
-        self.fetch = JSONFetcher().fetch
+        self.fetch = JSONFetcher(api_url).fetch
         
-    def get_orders_on_page(self, page_num = 1):
-        query_url = '{0}?page={1}'.format(self.api_url, page_num)
-        return self.fetch(query_url)['orders']
+    def get_stats(self):
+        return self.fetch()['pagination']
     
-    def get_summary(self):
-        return self.fetch(self.api_url)['pagination']
+    def get_orders_on_page(self, page_num = 1):
+        return self.fetch({ 'page' : page_num })['orders']
     
     def get_all_orders(self):
-        order_summary = self.get_summary()
+        orders = []
+        stats = self.get_stats()
+        num_pages = math.ceil(stats['total'] / stats['per_page'])
+        
+        for pg_num in range(1, num_pages + 1):
+            orders += self.get_orders_on_page(pg_num)
+            
+        return orders
 
 class ShopifySolution():
     
     def __init__(self, api_url):
-        pass
-    
+        self.orders = [order for order in ShopifyAPI(api_url).get_all_orders()]
+        
     def print_result(self):
         pass
     
+api_url = 'https://backend-challenge-fall-2017.herokuapp.com/orders.json'
+    
+s = ShopifySolution(api_url)
